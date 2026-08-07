dbd5fe1a1069560a21df916668a9356176f3c78f
EricTheToon <EricTheToon@gmail.com>
2026-08-07 08:12:56 -0500

    fix tags and hazard tags using old booster ids

--- content/misc/hazard_tags.lua
+++ content/misc/hazard_tags.lua
@@ -1264,6 +1264,7 @@ SMODS.Tag {
   config = { hazard = true, rerolls = 2 },
   
   loc_vars = function(self, info_queue, tag)
+	info_queue[#info_queue + 1] = G.P_CENTERS.p_abn_calamity_mega
     return { vars = { tag.config.rerolls } }
   end,
   
@@ -1272,7 +1273,7 @@ SMODS.Tag {
             local lock = tag.ID
             G.CONTROLLER.locks[lock] = true
             tag:yep('+', G.C.SECONDARY_SET.Spectral, function()
-                local booster = SMODS.create_card { key = 'p_abn_calamitybooster_m', area = G.play }
+                local booster = SMODS.create_card { key = 'p_abn_calamity_mega', area = G.play }
                 booster.T.x = G.play.T.x + G.play.T.w / 2 - G.CARD_W * 1.27 / 2
                 booster.T.y = G.play.T.y + G.play.T.h / 2 - G.CARD_H * 1.27 / 2
                 booster.T.w = G.CARD_W * 1.27
